93a1bcebe4aa662b56878a10b946f65c44c004a5
jquebec31 <lhenn3790@gmail.com>
2021-11-06 12:14:21 -0400

    added axios, integrated api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [searchword, setSearchword] = useState("");
     
-    function search(event) {
-        event.preventDefault();
-        alert(`Searching definition for ${searchword}`);
+    function handleResponse(response) {
+        console.log(response.data[0]);
     }
 
+    function search(event) {
+        event.preventDefault();
+       // alert(`Searching definition for ${searchword}`);
+    
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_us/${searchword}`;
+        axios.get(apiUrl).then(handleResponse);
+    }
     function handleSearchwordChange(event) {
         setSearchword(event.target.value);
     }
